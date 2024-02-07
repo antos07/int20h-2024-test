64948f27266150c29bc83fad4b12c81774833a24
antos07 <trotsenko.ant07@gmail.com>
2024-02-07 14:44:48 +0100

    Create an index page for auctions

--- frontend/src/components/AuctionsIndex.jsx
+++ frontend/src/components/AuctionsIndex.jsx
@@ -1,3 +1,96 @@
-export const AuctionsIndex = () => (
-    <h1>Auctions</h1>
-)
+import {Card, CardActionArea, CardContent, CardHeader, CardMedia, Typography} from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2";
+import {useEffect, useState} from "react";
+
+function AuctionCard({title, description, image, start_date, end_date, author}) {
+    const localizeDate = (date) => date && date.toLocaleDateString(undefined, {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric'
+    });
+
+    return <Card sx={{height: '500px'}}>
+        <CardActionArea>
+            <CardHeader
+                title={title}
+                subheader={<>
+                    <Typography>@{author}</Typography>
+                    <Typography variant={'subtitle2'}>
+                        {localizeDate(start_date)} - {localizeDate(end_date)}
+                    </Typography>
+                </>}
+            />
+            <CardMedia
+                sx={{height: 250, objectFit: "contain"}}
+                image={image}/>
+            <CardContent>
+                <Typography variant="body2" color={"text.secondary"}>
+                    {description}
+                </Typography>
+            </CardContent>
+        </CardActionArea>
+    </Card>
+}
+
+export const AuctionsIndex = () => {
+    const [auctions, setAuctions] = useState([]);
+
+    useEffect(() => {
+        setAuctions([
+            {
+                id: 1,
+                title: "Auction 1",
+                description: "Some cool auction",
+                start_date: new Date(),
+                end_date: new Date(),
+                author: "antos07",
+                image: "https://broken.image"
+            },
+            {
+                id: 2,
+                title: "Auction 2",
+                description: "Another reaaaaaaaaaaaaaaaally cool auction",
+                start_date: new Date(),
+                end_date: new Date(),
+                author: "SiIence_o0",
+                image: "https://t2.gstatic.com/licensed-image?q=tbn:ANd9GcQOO0X7mMnoYz-e9Zdc6Pe6Wz7Ow1DcvhEiaex5aSv6QJDoCtcooqA7UUbjrphvjlIc"
+            },
+            {
+                id: 3,
+                title: "Auction 3",
+                description: "I want to call my database API to information regarding a users public_address(metamask), first_name and last_name.\n" +
+                    "\n" +
+                    "Once I have this data (I call an api), I then use the mapping function in a list and then generate ListButtonItems for each element in my list.\n" +
+                    "\n" +
+                    "What I have done\n" +
+                    "\n" +
+                    "My API call is working perfectly fine, but the issue is my result array isn't populating, for example, when I retrieve my response (response is successfully fetched from the API), I add all the contents from my response to an array variable, this array always however appear s empty (I think this is due to the async nature of axios)\n" +
+                    "\n" +
+                    "Code",
+                start_date: new Date(),
+                end_date: new Date(),
+                author: "SiIence_o0",
+                image: "https://t2.gstatic.com/licensed-image?q=tbn:ANd9GcQOO0X7mMnoYz-e9Zdc6Pe6Wz7Ow1DcvhEiaex5aSv6QJDoCtcooqA7UUbjrphvjlIc"
+            }
+        ])
+    }, []);
+
+    return (
+        <Grid container spacing={2} sx={{mx: 'auto', mt: 1}}>
+            {
+                auctions.map((auction) => <Grid xs={12} sm={6} md={4} lg={3} xl={2}>
+                    <AuctionCard
+                        title={auction.title}
+                        description={auction.description}
+                        start_date={auction.start_date}
+                        end_date={auction.end_date}
+                        author={auction.author}
+                        image={auction.image}
+                    />
+                </Grid>)
+            }
+        </Grid>
+    );
+}
